8e2ddd2d85b6f05cbd89ac05f925a4953decfb86
Nick Dell'Aquilo <nickdellaquilo@proton.me>
2025-08-05 19:49:08 -0400

    add split_nodes_delimiter and tests

---
+++ src/split_nodes_delimiter.py
@@ -0,0 +1,25 @@
+from textnode import TextNode, TextType
+
+def split_nodes_delimiter(old_nodes: [TextNode], delimiter, text_type: TextType):
+    result = []
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            result.append(node)
+            continue
+
+        parts = node.text.split(delimiter)
+        if len(parts) == 1:
+            result.append(node)
+            continue
+
+        if len(parts) % 2 == 0:
+            raise Exception(
+                f"Invalid Markdown syntax: unmatched delimiter '{delimiter}' in text '{node.text}'"
+            )
+
+        for i, part in enumerate(parts):
+            if i % 2 == 0:
+                result.append(TextNode(part, TextType.TEXT))
+            else:
+                result.append(TextNode(part, text_type))
+    return result
---
+++ src/test_split_nodes_delimiter.py
@@ -0,0 +1,47 @@
+import unittest
+from textnode import TextNode, TextType
+from split_nodes_delimiter import split_nodes_delimiter
+
+class TestSplitNodesDelimiter(unittest.TestCase):
+    def test_no_delimiter(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+        self.assertEqual(new_nodes, [node])
+
+    def test_single_delimiter(self):
+        node = TextNode("This is a `code block", TextType.TEXT)
+        with self.assertRaises(Exception) as context:
+            split_nodes_delimiter([node], "`", TextType.CODE)
+        self.assertIn("unmatched delimiter", str(context.exception))
+
+    def test_multiple_delimiters(self):
+        node = TextNode("This is text with a `code block` and more text", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+        expected_nodes = [
+            TextNode("This is text with a ", TextType.TEXT),
+            TextNode("code block", TextType.CODE),
+            TextNode(" and more text", TextType.TEXT)
+        ]
+        self.assertEqual(new_nodes, expected_nodes)
+
+    def test_consecutive_delimiters(self):
+        node = TextNode("Text with ``empty`` code", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+        expected_nodes = [
+            TextNode("Text with ", TextType.TEXT),
+            TextNode("", TextType.CODE),
+            TextNode("empty", TextType.TEXT),
+            TextNode("", TextType.CODE),
+            TextNode(" code", TextType.TEXT)
+        ]
+        self.assertEqual(new_nodes, expected_nodes)
+
+    def test_non_text_node(self):
+        node = TextNode("This is bold text", TextType.BOLD)
+        new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+        self.assertEqual(new_nodes, [node])
+
+
+
+if __name__ == "__main__":
+    unittest.main()
