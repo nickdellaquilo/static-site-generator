07eaa83e39f953cfc79a154ce19f8bf6eee060a9
Nick Dell'Aquilo <nickdellaquilo@proton.me>
2025-09-05 17:28:09 -0400

    add text_to_textnodes and unit tests

--- src/test_split_nodes.py
+++ src/test_split_nodes.py
@@ -1,6 +1,6 @@
 import unittest
 from textnode import TextNode, TextType
-from split_nodes import split_nodes_delimiter, split_nodes_image, split_nodes_link
+from split_nodes import split_nodes_delimiter, split_nodes_image, split_nodes_link, text_to_textnodes
 
 class TestSplitNodesDelimiter(unittest.TestCase):
     def test_no_delimiter(self):
@@ -29,9 +29,7 @@ class TestSplitNodesDelimiter(unittest.TestCase):
         new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
         expected_nodes = [
             TextNode("Text with ", TextType.TEXT),
-            TextNode("", TextType.CODE),
             TextNode("empty", TextType.TEXT),
-            TextNode("", TextType.CODE),
             TextNode(" code", TextType.TEXT)
         ]
         self.assertEqual(new_nodes, expected_nodes)
@@ -127,16 +125,79 @@ class TestSplitNodesDelimiter(unittest.TestCase):
             new_nodes,
         )
 
-    # def test_split_links_invalid_markdown(self):
-    #     node = TextNode(
-    #         "This is text with an [invalid link](https://boot.dev without closing parenthesis",
-    #         TextType.TEXT,
-    #     )
-    #     with self.assertRaises(ValueError) as context:
-    #         split_nodes_link([node])
-    #     self.assertIn("invalid markdown, link section not closed", str(context.exception))
+    def test_text_to_textnodes(self):
+        # Test text_to_textnodes function
+        text = "This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
+        nodes = text_to_textnodes(text)
+        expected_nodes = [
+            TextNode("This is ", TextType.TEXT),
+            TextNode("text", TextType.BOLD),
+            TextNode(" with an ", TextType.TEXT),
+            TextNode("italic", TextType.ITALIC),
+            TextNode(" word and a ", TextType.TEXT),
+            TextNode("code block", TextType.CODE),
+            TextNode(" and an ", TextType.TEXT),
+            TextNode("obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"),
+            TextNode(" and a ", TextType.TEXT),
+            TextNode("link", TextType.LINK, "https://boot.dev"),
+        ]
+        self.assertEqual(nodes, expected_nodes)
 
+    def test_text_to_textnodes_no_formatting(self):
+        text = "This is plain text without any formatting."
+        nodes = text_to_textnodes(text)
+        expected_nodes = [TextNode("This is plain text without any formatting.", TextType.TEXT)]
+        self.assertEqual(nodes, expected_nodes)
 
+    def test_text_to_textnodes_only_formatting(self):
+        text = "**Bold** _Italic_ `Code` ![Image](https://example.com/image.png) [Link](https://example.com)"
+        nodes = text_to_textnodes(text)
+        expected_nodes = [
+            TextNode("Bold", TextType.BOLD),
+            TextNode(" ", TextType.TEXT),
+            TextNode("Italic", TextType.ITALIC),
+            TextNode(" ", TextType.TEXT),
+            TextNode("Code", TextType.CODE),
+            TextNode(" ", TextType.TEXT),
+            TextNode("Image", TextType.IMAGE, "https://example.com/image.png"),
+            TextNode(" ", TextType.TEXT),
+            TextNode("Link", TextType.LINK, "https://example.com"),
+        ]
+        self.assertEqual(nodes, expected_nodes)
+
+    def test_text_to_textnodes_unmatched_delimiter(self):
+        text = "This is **bold text with no closing delimiter"
+        with self.assertRaises(Exception) as context:
+            text_to_textnodes(text)
+        self.assertIn("unmatched delimiter", str(context.exception))
+
+    def test_text_to_textnodes_empty_string(self):
+        text = ""
+        nodes = text_to_textnodes(text)
+        expected_nodes = [TextNode("", TextType.TEXT)]
+        self.assertEqual(nodes, expected_nodes)
+
+    def test_text_to_textnodes_adjacent_formatting(self):
+        text = "**Bold**_Italic_`Code`"
+        nodes = text_to_textnodes(text)
+        expected_nodes = [
+            TextNode("Bold", TextType.BOLD),
+            TextNode("Italic", TextType.ITALIC),
+            TextNode("Code", TextType.CODE),
+        ]
+        self.assertEqual(nodes, expected_nodes)
+
+    def test_text_to_textnodes_nested_formatting(self):
+        text = "This is **bold and _italic_** text"
+        nodes = text_to_textnodes(text)
+        expected_nodes = [
+            TextNode("This is ", TextType.TEXT),
+            TextNode("bold and _italic_", TextType.BOLD),
+            TextNode(" text", TextType.TEXT),
+        ]
+        self.assertEqual(nodes, expected_nodes)
+
+    
 
 if __name__ == "__main__":
     unittest.main()
