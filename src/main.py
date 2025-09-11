4966e3f7f051581b45c344924e9645c8944084e1
Nick Dell'Aquilo <nickdellaquilo@proton.me>
2025-09-11 16:44:49 -0400

    add transfer

--- src/main.py
+++ src/main.py
@@ -1,11 +1,20 @@
+import os
+import shutil
 from textnode import TextNode, TextType
 
 def main():
     test = TextNode("Hello there", TextType.LINK, "https://github.com/nickdellaquilo/")
     print(test)
+    transfer()
 
 
 
+def transfer(src="static", dst="public"):
+    if not os.path.exists(dst):
+        os.makedirs(dst)
+    shutil.rmtree(dst)
+    shutil.copytree(src, dst)
+    
 
 if __name__ == "__main__":
     main()
