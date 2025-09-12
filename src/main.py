bfe39f9291e4bc05e54d0ee8d833f1ae9c52092b
Nick Dell'Aquilo <nickdellaquilo@proton.me>
2025-09-12 14:18:07 -0400

    update main

--- src/main.py
+++ src/main.py
@@ -9,12 +9,12 @@ from textnode import TextNode, TextType
 def main():
     # test = TextNode("Hello there", TextType.LINK, "https://github.com/nickdellaquilo/")
     # print(test)
-    # transfer("static", "public")
-    # generate_page("content/index.md", "template.html", "public/index.html")
+
+    transfer("static", "docs")
 
     basepath = '/'
-    if sys.argv and len(sys.argv) > 0:
-        basepath = sys.argv[0]
+    if sys.argv and len(sys.argv) > 1:
+        basepath = sys.argv[1]
     generate_pages_recursive("content", "template.html", "docs", basepath)
 
 def generate_page(from_path, template_path, dest_path, basepath):
@@ -25,7 +25,7 @@ def generate_page(from_path, template_path, dest_path, basepath):
         template_content = f.read()
     html_content = markdown_to_html_node(markdown_content).to_html()
     title = extract_title(markdown_content)
-    result = template_content.replace("{{ Title }}", title).replace("{{ Content }}", str(html_content)).replace('href="/', f'href="{basepath}/').replace('src="/', f'src="{basepath}/')
+    result = template_content.replace("{{ Title }}", title).replace("{{ Content }}", str(html_content)).replace('href="/', f'href="{basepath}').replace('src="/', f'src="{basepath}')
     os.makedirs(os.path.dirname(dest_path), exist_ok=True)
     with open(dest_path, "w", encoding="utf-8") as f:
         f.write(result)
